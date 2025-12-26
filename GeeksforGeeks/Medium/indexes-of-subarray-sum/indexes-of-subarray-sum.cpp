bb4bc3f7efae3eda13040cd39aeb82c27f1b73f0
Bhuvana Korrapati <bhuvanakorrapati@gmail.com>
2025-12-25 20:57:18 -0500

    ✨ Add solution: Indexes of Subarray Sum

---
+++ GeeksforGeeks/Medium/indexes-of-subarray-sum/indexes-of-subarray-sum.cpp
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, arr, target):
+        left = 0
+        curr_sum = 0
+
+        for right in range(len(arr)):
+            curr_sum += arr[right]
+
+            while curr_sum > target and left <= right:
+                curr_sum -= arr[left]
+                left += 1
+
+            if curr_sum == target:
+                # return 1-based indices
+                return [left + 1, right + 1]
+
+        return [-1]
